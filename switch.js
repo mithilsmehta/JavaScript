dd351df363f0b11ae2bda0a8f55b02d3d0097e54
mithilmehta <mithilmehta15@gmail.com>
2025-01-02 15:58:35 +0530

    adding switch case control statement

---
+++ switch.js
@@ -0,0 +1,83 @@
+/*var month =1;
+
+switch(month)
+{
+    case 1: 
+    document.write("January");
+    break;
+
+    case 2: 
+    document.write("Febuary");
+    break;
+
+    case 3: 
+    document.write("March");
+    break;
+
+    case 4: 
+    document.write("April");
+    break;
+
+    case 5: 
+    document.write("May");
+    break;
+
+    case 6: 
+    document.write("June");
+    break;
+
+    case 7: 
+    document.write("July");
+    break;
+
+    case 8: 
+    document.write("August");
+    break;
+
+    case 9: 
+    document.write("September");
+    break;
+
+    case 10: 
+    document.write("October");
+    break;
+
+    case 11: 
+    document.write("November");
+    break;
+
+    case 12: 
+    document.write("December");
+    break;
+
+    default:
+    document.write("Wrong Input");
+
+}
+
+var month = "July";
+switch(month)
+{
+    case"June":
+    document.write("Sixth Month");
+    break;
+    case"July":
+    document.write("Seventh Month");
+    break;
+    default:
+        document.write("Wrong Input");
+}
+ // or true false also work for it
+
+ var month = true;
+ switch(month)
+ {
+     case true:
+     document.write(1);
+     break;
+     case false:
+     document.write(0);
+     break;
+     default:
+         document.write("Wrong Input");
+ }*/
