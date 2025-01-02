533a9e415b6b31b6ece3cd98f9256d2f99f84164
mithilmehta <mithilmehta15@gmail.com>
2025-01-02 14:45:35 +0530

    adding javaoperators

---
+++ Operator.js
@@ -0,0 +1,101 @@
+/*var x=6;
+var y=4;
+
+document.write("\n Addition\n ", x+y);
+document.write("\n Subtraction\n ", x-y);
+document.write("\n Divison\n ", x/y);
+document.write("\n modulus\n ", x%y);
+
+var z=5;
+document.write("\n Increment\n ", ++z);
+document.write("\n Decrement\n ", --z);
+
+var a= 13;
+var b= a;
+
+document.write("Value of b", b);
+
+var a1 =51;
+a1+=9;
+a1 = a1+9;
+document.write("\nvalue of a1\n", a1);
+
+a1-=5;
+document.write("\nvalue of a1\n", a1)
+
+a3 = 10;
+a3*=5;
+document.write("\n value of a3\n", a3)
+
+a4 = 16;
+a4/=2;
+document.write("\nvalue of a4\n", a4)
+
+a5 = 20;
+a5%= 7;
+document.write("\nvalue of a5\n", a5)
+
+var str1 = "Hello";
+var str2 = "World";
+document.write(str1+" "+str2);
+
+str3 = "\nWelcome";
+str3+= " "+ " to java";
+document.write(str3);
+
+var a=12;
+var b=8;
+var c=5;
+var d=10;
+var result= a+b+"hello"+c+d;
+document.write("<h1>Value of result is \n"+result+"\nHi there</h1>\n");
+
+var x=60;
+var y= document.write("Value is \n", x>=50?"Greater":"Smaller");
+
+var x=10;
+
+document.write(x==8);
+document.write("<br>")
+document.write(x===8);
+document.write("<br>")
+document.write(x!=8);
+document.write("<br>")
+document.write(x!==8);
+document.write("<br>")
+document.write(x>8);
+document.write("<br>")
+document.write(x<8);
+document.write("<br>")
+document.write(x>=8);
+document.write("<br>")
+document.write(x<=8);
+
+var x =17;
+var y= 'Hey there!';
+var flag = false;
+var p;
+var abc = null;
+
+document.write(typeof (x));
+document.write("<br>")
+document.write(typeof (y));
+document.write("<br>")
+document.write(typeof (flag));
+document.write("<br>")
+document.write(typeof (p));
+document.write("<br>")
+document.write(typeof (abc));
+document.write("<br>")
+
+var x =5;
+var y =1;
+
+document.write(x&y);
+document.write("<br>")
+document.write(x|y);
+document.write("<br>")
+document.write(~x);
+document.write("<br>")
+document.write(x^y);
+document.write("<br>")*/
