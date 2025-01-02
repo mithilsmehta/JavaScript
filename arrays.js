45cb90c703827c46b875e4ce97fdc1c0bccac777
mithilmehta <mithilmehta15@gmail.com>
2025-01-02 17:28:20 +0530

    adding arrays

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var car =["Honda", "Kia", "Ford"]
+car.push("Nissan");
+car.pop();
+car.pop();
+car.push("Hyundai");
+car.push(20);
+for(var i = 0;i<car.length;i++)
+{
+document.write("<h1>"+car[i]+"</h1>")
+}
+
+
+/*var fruits =new Array("Apple", "Banana", "Mango");
+for(var i = 0;i<3;i++)
+{
+document.write("<h1>"+fruits[i]+"</h1>")
+}*/
+
+
+
+
+
+
+/*var car1="Honda";
+var car2="Kia";
+var car3="Ford";
+
+document.write("<h1>"+car1+"</h1>")
+document.write("<h1>"+car2+"</h1>")
+document.write("<h1>"+car3+"</h1>")*/
