fb99660bf32224ca6c08d95481cc2211507d21e8
mithilmehta <mithilmehta15@gmail.com>
2025-01-02 16:17:02 +0530

    adding functions

---
+++ functions.js
@@ -0,0 +1,20 @@
+/*function sum(x,y)
+{
+   
+    var total = x+y;
+
+    document.write(total);
+}
+
+sum(4,3);
+
+// or 
+
+function sum(x,y)
+{
+    var total = x+y;
+    return total;
+}
+
+var output = sum(35,15);
+document.write(output);*/
