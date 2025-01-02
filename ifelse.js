d1c4923bffebae390ac1936224ddf6dac7683e4b
mithilmehta <mithilmehta15@gmail.com>
2025-01-02 15:30:05 +0530

    adding if else statement

---
+++ ifelse.js
@@ -0,0 +1,91 @@
+/*var x = 8;
+if (x<10)
+{
+    document.write("the value of x is less than 10");
+}
+else if (x=10)
+{
+    document.write("the value of x is equals to 10");
+}
+else 
+{
+    document.write("the value of x is greater than 10");
+}
+
+var x=8;
+if (x%2==0)
+{
+    document.write("Even Number");
+}
+else
+{
+    document.write("Odd Number");
+}
+
+var y = 0;
+if(y>0)
+{
+    document.write("Positive Number");
+}
+else if(y<0)
+{
+    document.write("Negative Number");
+}
+else
+{
+    document.write("zero");
+}
+
+var x = 7;
+if(x>0)
+{if (x%2==0)
+    {
+    document.write("Positive and Even");
+    }
+else 
+    {
+    document.write("Positive and Odd");
+    }
+}
+else if(x<0)
+{
+    if (x%2==0)
+        {
+        document.write("Negative and Even");
+        }
+    else 
+    {
+        document.write("Negative and Odd");
+    }
+}
+else
+{
+    document.write("zero");
+}
+
+var x = 7;
+if(x>0 && x%2==0)
+
+    {
+    document.write("Number is Positive and Even");
+    }
+else if(x>0 && x%2!=0)
+    {
+    document.write("Number is Positive and Odd");
+    }
+
+else if(x<0 && x%2==0)
+
+     {
+        document.write("Number is Negative and Even");
+        }
+else if (x<0 && x%2!=0)
+    {
+        document.write("Number is Negative and Odd");
+    }
+
+else
+{
+    document.write("zero");
+}*/
+
