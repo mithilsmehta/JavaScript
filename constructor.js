ab3cceb1242af6f6b4057f523297a724f4852850
mithilmehta <mithilmehta15@gmail.com>
2025-01-03 13:15:10 +0530

    adding constructor

---
+++ constructor.js
@@ -0,0 +1,44 @@
+// var obj_name = new Object();
+
+/*var rect2 = new Object();//var rect1 = {};
+rect2.length = 5;
+rect2.width =10;
+
+rect2.getarea = function(){
+    return this.length*this.width;
+}
+
+rect2.getperimeter = function(){
+    return 2*this.length + 2*this.width;
+}
+
+document.write("<h2>"+"The length of Rectangle is"+ rect2.length+"</h2>");
+document.write("<h2>"+"The Width of Rectangle is"+ rect2.width+"</h2>");
+document.write("<h2>"+"The Area of Rectangle is"+ rect2.getarea()+"</h2>");
+document.write("<h2>"+"The Parameter of Rectangle is"+ rect2.getperimeter()+"</h2>");
+
+function rect3(length,width)
+{
+    this.length = length;
+    this.width = width;
+    this.getarea = function(){
+      return length*width;
+    }
+
+    this.getperimeter = function(){
+        return 2*length+ 2*width;
+    }
+}
+var r1 = new rect3(5,10);
+document.write("<h1>"+r1.length+"</h1>")
+document.write("<h1>"+r1.width+"</h1>")
+document.write("<h1>"+"The area of Rectangle is "+r1.getarea()+"</h1>")
+document.write("<h1>"+"The parameter of Rectangle is "+r1.getperimeter()+"</h1>")
+
+var r2 = new rect3(4,8);
+document.write("<h1>"+r2.length+"</h1>")
+document.write("<h1>"+r2.width+"</h1>")
+document.write("<h1>"+"The area of Rectangle is "+r2.getarea()+"</h1>")
+document.write("<h1>"+"The parameter of Rectangle is "+r2.getperimeter()+"</h1>")*/
+
+
