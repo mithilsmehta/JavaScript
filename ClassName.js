52c16fe8de22ac9d8c1e794890c29cc32017533a
mithilmehta <mithilmehta15@gmail.com>
2025-01-04 12:52:02 +0530

    adding classname

---
+++ ClassName.js
@@ -0,0 +1,7 @@
+function func1(){
+    var x = document.getElementsByClassName("cls1");
+    console.log(x);
+    x[0].style.fontStyle = "italic";
+    x[1].style.color = "red";
+    x[2].style.fontWeight = "bolder";
+}
