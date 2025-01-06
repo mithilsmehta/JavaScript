4d56791da6c4a0ffce46b877701d1ab80d9d5583
mithilmehta <mithilmehta15@gmail.com>
2025-01-06 11:43:19 +0530

    adding arrowfunctions

---
+++ arrowfunctions.js
@@ -0,0 +1,18 @@
+/*function func1(a,b){
+    return a+b;
+}
+var arrow1 = (a,b) => a+b;*/
+function func2(){
+    console.log("Welcome to JavaScript");
+}
+func2();
+
+var arrow2 = () => {
+    console.log("Welcome to JS");
+}
+arrow2();
+
+var arrow3 = newname => {
+    console.log("Hello World"+ newname);
+}
+arrow3("!");
