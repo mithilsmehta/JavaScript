b81c9589512efdf59ea759f47ddf4ef8f064ac8f
mithilmehta <mithilmehta15@gmail.com>
2025-01-06 17:50:39 +0530

    adding maps

---
+++ maps.js
@@ -0,0 +1,56 @@
+/*var myCar = new Object()
+{
+    myCar.manufacturer = "Ford";
+    myCar.name = "Raptor";
+    myCar.color = "Black";
+}
+myCar.make ="2012";
+console.log(myCar);
+
+var myCar = new Object()
+{
+
+}
+
+console.log(myCar);
+
+var myCarr = new Map()
+{
+ myCarr.set(1,"BMW")
+ myCarr.set(2,"Audi")
+ myCarr.set(3,"Mercedes")
+}
+console.log(myCarr.size);
+//or
+
+var myCar = new Object()
+{
+
+}
+
+console.log(myCar);
+
+var myCarr = new Map()
+let obj = {};
+let func = function() {}
+{
+ myCarr.set("My car Name is ", "BMW");
+ myCarr.set(obj,"Honda");
+ myCarr.set(func, "Mercedes");
+}
+console.log(myCarr.get(obj));
+//or
+var myCar = new Object()
+{
+
+}
+
+console.log(myCar);
+
+var myarr = [[1,"BMW"],[2,"Audi"],[3,"Honda"]]
+var myCarr = new Map(myarr) 
+
+var yourCar = new Map(myCarr)
+console.log(yourCar)
+
+console.log(myCarr === yourCar);*/
