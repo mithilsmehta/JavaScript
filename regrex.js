b8adb11ef0aa7a4136475a0d7b69a6a1db4f83bb
mithilmehta <mithilmehta15@gmail.com>
2025-01-06 14:06:39 +0530

    adding regrex

---
+++ regrex.js
@@ -0,0 +1,22 @@
+var regrex = /Hi/g
+var str1 = "Hello and Welcome to our Youtube channel.Hello if you are new here and Hello "
+
+//exec()
+
+/*var output = regrex.exec(str1)
+console.log(output)
+var output = regrex.exec(str1)
+console.log(output)*/ 
+
+//test()
+
+//var output = regrex.test(str1)
+//console.log(output)
+
+//match()
+//var output = str1.match(regrex);
+//console.log(output);
+
+//replace()
+//var output = str1.replace(regrex, "Hi");
+//console.log(output);
