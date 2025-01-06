e07cee047195e96c19fa08d55d2561eac95a8172
mithilmehta <mithilmehta15@gmail.com>
2025-01-06 16:57:27 +0530

    adding debugging

---
+++ debugging.js
@@ -0,0 +1,12 @@
+function add()
+{
+    var no1 = parseInt(document.getElementById("n1").value);
+    var no2 = parseInt(document.getElementById("n2").value);
+console.log("The sum is "+ parseInt(no1+no2));
+
+
+
+   // var no3 = no1 + no2;
+   // alert("The Sum is" + no3);
+    
+}
