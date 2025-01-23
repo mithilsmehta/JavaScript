a37a280b36f52da6fc1dfb158fdda6a2aa75dc8f
mithilmehta <mithilmehta15@gmail.com>
2025-01-23 14:08:15 +0530

    Adding React App and understanding props and learn Navbar and added bootstraps

---
+++ React/myapp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+// let name ="React";
+// let rollno ="25";
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+{/* <Navbar title = "MyApp" /> */}
+{/* <Navbar/> */}
+<Navbar title = "MyApp"/>
+
+    </>
+  );
+}
+
+export default App;   
