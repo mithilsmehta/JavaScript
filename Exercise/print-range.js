5abff541fcb802d7a484cca95031cb008b906c33
mithilmehta <mithilmehta15@gmail.com>
2025-01-29 16:50:55 +0530

    pratice of JS

--- Exercise/print-range.js
+++ Exercise/print-range.js
@@ -1,14 +1,23 @@
-function printRange(num){
-     console.log(num);
-     var i = 0;
-     while (i < num) {
-        console.log(i); 
-        i=i+1;
-     }
+// function printRange(num){
+//      console.log(num);
+function parameters(startNum, endNum) {
+   let num = startNum; 
+
+   while (num < endNum) { 
+       console.log(num);
+       num++; 
+   }
 }
+parameters(14, 27); 
+parameters(1000, 1005); 
+parameters(3200, 3203); 
+parameters(4700, 4704); 
+parameters(6150, 6156); 
+
+// }
 // printRange(8);
 // printRange(3,8);
-printRange(14,27);
+// printRange(14,27);
 // printRange(9);
 // num = 9
 // console.log(0);
@@ -20,4 +29,12 @@ printRange(14,27);
 // console.log(6);
 // console.log(7);
 // console.log(8);
+// new task using parameters
+// 14,27
+// 1000,1005
+// 3200,3203
+// 4700,4704
+// 6150,6156
+
+
 
