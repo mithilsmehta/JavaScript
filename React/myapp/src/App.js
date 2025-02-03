18c4d74ea59c0fcdc1c2a4c6f971bd0da7f84377
mithilmehta <mithilmehta15@gmail.com>
2025-02-03 20:27:41 +0530

    added new components and new features and edited logo and app name

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -1,18 +1,69 @@
 
 import './App.css';
-// let name ="React";
-// let rollno ="25";
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+
+
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been Enabled", "success");
+      // document.title = 'My App - Dark Mode'; this if for changing the title of app 
+      // setInterval(() => {
+      //   document.title = 'My App is Amazing';
+      // }, 2000);
+      // setInterval(() =>{
+      //   document.title = "Install My App";
+      // },1500);
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been Enabled", "success");
+      // document.title = 'My App - Light Mode'; this is for changing the title of app 
+    }
+  }
   return (
     <>
-{/* <Navbar title = "MyApp" /> */}
-{/* <Navbar/> */}
-<Navbar title = "MyApp"/>
-<div className='container'>
-<TextForm heading ="Enter the Text to Analyze"/>
-</div>
+      {/* <Navbar title = "MyApp" aboutText = "About"/> */}
+      {/* <Navbar/> */}
+
+      <Navbar title="MyApp" mode={mode} aboutText="About" toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <div className='container'>
+
+        <About />
+        <TextForm showAlert={showAlert} heading="Enter the Text to analyze below" mode={mode} />
+
+      </div>
+
+
     </>
   );
 }
--- React/myapp/src/components/TextForm.js
+++ React/myapp/src/components/TextForm.js
@@ -1,16 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was Clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+        // props.showAlert("Converted to Uppercase!", "Sucess");
+    }
+    const handleLoClick = ()=>{
+        // console.log("Lowercase was Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+        // props.showAlert("converted to Lowercase!", "Success");
+    }
+    const handleClearClick = ()=>{
+        let newText = "";
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{
+        // console.log("On Change");
+        setText(event.target.value)
+    }
+
+    const handleCopy = ()=>{
+        var text = document.getElementById("mybox");
+        text.select();
+        text.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+    }
+  
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    const [text, setText] = useState('');
+    // text = "new text";  wrong way to change the state
+    // setText = ("new text"); correct way to change the state 
   return (
-    <div>
+    <>   
+    <div className="container" style={{color: props.mode=== 'dark'?'white':'#042743'}}>
+     <div>
         <h1>{props.heading}</h1>
-  <div className="mb-3">
-    <textarea className="form-control" id="mybox" rows="8"/>
-    <br />
-    <button className="btn btn-primary">Convert to Uppercase</button>
-   
-    
-  </div>
+    <div className="mb-3">
+    <textarea className="form-control" value ={text} onChange={handleOnChange} style={{backgroundColor: props.mode=== 'dark'?'grey':'white', color: props.mode=== 'dark'?'white':'#042743' }} id="mybox" rows="8"> </textarea>
     </div>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
+    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button> 
+    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button> 
+
+     </div>
+    </div>
+    <div className="container my-3" style={{color: props.mode=== 'dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        {/* <p>{text.split(" ").length} words and {text.length} characters</p> this line is for word counting but there is a bug in this line so i have written new line below. */}
+        <p>{text.split(" ").filter(word => word.trim() !== "").length} words, {text.length} Characters</p> 
+        <p>{0.008 * text.split(" ").length} Minutes to Read</p>
+        <h3>Preview</h3>
+        <p>{text.length>0?text:"Enter Something in the above textbox to preview here"}</p>
+    </div>
+    </>
+
   )
 }
