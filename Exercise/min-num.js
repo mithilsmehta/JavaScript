9ccc4e3203d82fc5d13c213aeb54ef6204a0ca56
mithilmehta <mithilmehta15@gmail.com>
2025-02-07 16:50:15 +0530

    printed index value and return index and that value

--- Exercise/min-num.js
+++ Exercise/min-num.js
@@ -14,5 +14,34 @@ function findMin(arr) {
     }
     console.log(min);
 }
-let numbers = [1, 5, 4, 3, 0];
-findMin(numbers);
+// let numbers = [1, 5, 4, 0, 3];
+// findMin(numbers);
+
+// index print krvanu value print krvani index ne return krvanu 
+// findIndexOfMinValue
+function findIndexOfMinValue(arr) {
+        if (arr.length === 0) 
+            return;
+
+        let index = 0;
+        let minIndex=0;
+        let minValue = arr[0];
+
+    
+        while (index < arr.length){
+            if (arr[index] < minValue){
+                minValue = arr[index];
+                minIndex = index;
+            }
+            index++;
+        }
+        console.log(`Min Index: ${minIndex}, Min Value: ${minValue}`);
+        return minIndex;
+}
+
+let arr = [6,2,3,7,10,1,9];
+let minIndex = findIndexOfMinValue(arr);
+
+
+
+
